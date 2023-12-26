82b3d594ac6fc8558fcb04ff3f23c6970e05bd29
Adi <adityasagarpython@gmail.com>
2023-12-26 19:11:10 -0400

    Skeleton AI completed:  Patrol and Attack

--- Source/Platformer/Characters/Skel.cpp
+++ Source/Platformer/Characters/Skel.cpp
@@ -2,15 +2,27 @@
 
 
 #include "Skel.h"
+#include "Perception/AIPerceptionStimuliSourceComponent.h"
+#include "Perception/AISense_Sight.h"
 
 // Sets default values
 ASkel::ASkel()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-	
+	SetupStimulusSource();
 }
 
+void ASkel::SetupStimulusSource()
+{
+	StimulusSource = CreateDefaultSubobject<UAIPerceptionStimuliSourceComponent>(TEXT("Stimulus"));
+	if (StimulusSource)
+	{
+		StimulusSource->RegisterForSense(TSubclassOf<UAISense_Sight>());
+		StimulusSource->RegisterWithPerceptionSystem();
+	}
+
+}
 // Called when the game starts or when spawned
 void ASkel::BeginPlay()
 {
